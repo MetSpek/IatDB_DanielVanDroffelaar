7a4e67d9cdac9915f42b6cd6fa4d0f93868d53fe
Daniel van Droffelaar <daniel@realafford.nl>
2021-06-03 23:21:01 +0200

    oke, nu ben ik klaar

--- database/migrations/2021_06_01_144823_create_reacties_table.php
+++ database/migrations/2021_06_01_144823_create_reacties_table.php
@@ -16,12 +16,12 @@ class CreateReactiesTable extends Migration
         Schema::create('reacties', function (Blueprint $table) {
             $table->id('verzoek_id');
             $table->foreignId('eigenaar');
-            $table->foreign('eigenaar')->references('eigenaar')->on('dieren');
+            $table->foreign('eigenaar')->references('eigenaar')->on('dieren')->onDelete('cascade');;
             $table->foreignId('zoeker');
-            $table->foreign('zoeker')->references('id')->on('users');
+            $table->foreign('zoeker')->references('id')->on('users')->onDelete('cascade');;
             $table->string('zoeker_naam');
             $table->foreignId("dier");
-            $table->foreign('dier')->references('number')->on('dieren');
+            $table->foreign('dier')->references('number')->on('dieren')->onDelete('cascade');;
             $table->string('dier_naam');
         });
     }
